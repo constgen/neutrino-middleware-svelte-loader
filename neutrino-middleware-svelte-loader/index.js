8505eb1e8b939077a9dbe45e74200cb90e7a2c46
сgenchevsky <const.gen@gmail.com>
2017-04-01 21:03:07 +0300

    Add new 'svlt' extension

--- neutrino-middleware-svelte-loader/index.js
+++ neutrino-middleware-svelte-loader/index.js
@@ -4,19 +4,19 @@ let path = require('path')
 
 module.exports = function (neutrino, options) {
 	const NODE_MODULES = path.join(__dirname, 'node_modules')
-	const RULE_EXTENSIONS = /\.(html?|svelte)$/
+	const LOADER_EXTENSIONS = /\.(html?|svelte|svlt)$/
 	let config = neutrino.config
 	let compileRule = config.module.rules.get('compile')
 	let compileRuleExtensions = compileRule && compileRule.get('test') || []
 	compileRuleExtensions = (compileRuleExtensions instanceof Array) ? compileRuleExtensions : [compileRuleExtensions]
-	let commonRuleExtensions = compileRuleExtensions.concat(RULE_EXTENSIONS)
+	let commonRuleExtensions = compileRuleExtensions.concat(LOADER_EXTENSIONS)
 
 	options = options || {}
 	config.module.rule('compile')
 		.test(commonRuleExtensions)
 
 	config.module.rule('svelte')
-		.test(RULE_EXTENSIONS)
+		.test(LOADER_EXTENSIONS)
 		.include
 			.merge(options.include || [])
 			.end()
